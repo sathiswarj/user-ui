3e146c9d020c4a0da20ea7f11cdf76eb987b006c
Sathiswarj <slsjkr@gmail.com>
2025-11-07 21:13:53 +0530

    server updated

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const jsonServer = require('json-server');
-const cors = require('cors');  
+const cors = require('cors');
 
 const app = express();
 
-  app.use(cors());
+ app.use(cors());
 
  const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -14,10 +14,10 @@ app.use('/', middlewares, router);
  app.use(express.static(path.join(__dirname, 'build')));
 
  app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'), (err) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'), err => {
     if (err) res.status(500).send(err);
   });
 });
 
-const PORT = process.env.PORT || 5000;
+ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
